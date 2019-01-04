465e955065f5c9f6b278eb8c24881573c4c20d67
priandey <riandeypierre@gmail.com>
2019-01-04 05:35:09 +0100

    Some arch and pseudocode

---
+++ main.py
@@ -0,0 +1,7 @@
+import models as m
+
+def main():
+    # Initialize maze
+    # Get input from user : number of steps & number of obstacles
+    # Magic tric
+    # Output = list of obstacle's coordinates
