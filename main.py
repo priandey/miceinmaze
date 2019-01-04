9cf6c2b921b1b28c110b1434069121cd24a0f6d6
Hornycar <41443504+Hornycar@users.noreply.github.com>
2019-01-04 11:45:17 +0100

    adding UTF8 encoding

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*
+
 import models as m
 
 def main():
     maze = m.Maze()
     maze.std_structure()
-    maze.print_structure(True) # Passer "True" en paramètre pour afficher les coordonnées de chaque passage au lieu de '#'
+    maze.print_structure(True) # Passer "True" en parametre pour afficher les coordonnees de chaque passage au lieu de '#'
     maze.get_end_point()
     maze.print_structure()
     # Initialize maze
--- models/maze.py
+++ models/maze.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*
+
 class Maze():
     def __init__(self):
         self.structure = list()
