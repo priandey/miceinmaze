1fbc54d2fa593a1e4ddc50d9072a2c7f8ddd5e2e
Hornycar <41443504+Hornycar@users.noreply.github.com>
2019-01-04 11:56:37 +0100

    I don't know where this is coming from :'(

--- main.py
+++ main.py
@@ -5,15 +5,10 @@ import models as m
 def main():
     maze = m.Maze()
     maze.std_structure()
-<<<<<<< HEAD
-<<<<<<< HEAD
     maze.print_structure(True) # Passer "True" en parametre pour afficher les coordonnees de chaque passage au lieu de '#'
-=======
-    maze.print_structure(True) # Passer "True" en parametre pour afficher les coordonnées de chaque passage au lieu de '#'
->>>>>>> 787b5cbdcb66ca1ec7da031b9d58d2a063d8b93b
-=======
+
     #maze.print_structure(True) # Passer "True" en parametre pour afficher les coordonnées de chaque passage au lieu de '#'
->>>>>>> 4a1a49fc67d7220bc6278bc9ab5b553343add77c
+
     maze.get_end_point()
     #maze.print_structure()
     mice = m.Mice(maze)
