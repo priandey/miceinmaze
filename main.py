4a1a49fc67d7220bc6278bc9ab5b553343add77c
priandey <riandeypierre@gmail.com>
2019-01-04 11:49:49 +0100

    mice now moving randomly

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import models as m
 def main():
     maze = m.Maze()
     maze.std_structure()
-    maze.print_structure(True) # Passer "True" en parametre pour afficher les coordonnées de chaque passage au lieu de '#'
+    #maze.print_structure(True) # Passer "True" en parametre pour afficher les coordonnées de chaque passage au lieu de '#'
     maze.get_end_point()
-    maze.print_structure()
+    #maze.print_structure()
+    mice = m.Mice(maze)
+    result = mice.get_path()
+    print(result)
     # Initialize maze
     # Get input from user : number of steps & number of obstacles & endpoint
     # Magic tric
--- models/mice.py
+++ models/mice.py
@@ -14,11 +14,11 @@ class Mice():
         condition = False
         while not condition:
             self.current_position = self.start_point
-            for steps in range(motion_limit):
-
+            while len(path) != motion_limit:
+                adding = False
                 potential_steps = list()
 
-                for line in self.maze.structure
+                for line in self.maze.structure:
                     if (self.current_position[0]+1,self.current_position[1]) in line:
                         potential_steps.append((self.current_position[0]+1,self.current_position[1]))
                     if (self.current_position[0],self.current_position[1]+1) in line:
@@ -28,14 +28,18 @@ class Mice():
                     if (self.current_position[0],self.current_position[1]-1) in line:
                         potential_steps.append((self.current_position[0],self.current_position[1]-1))
                     else:
-                        break
+                        continue
 
                 decided_step = choice(potential_steps)
 
                 if decided_step in self.maze.wall_coord or decided_step in self.maze.obstacle_coord:
-                    break
+                    continue
+                else:
+                    self.current_position = decided_step
+                    path.append(decided_step)
+                    condition = True
 
-
+        return path
                 # move randomly 1 steps if step in Maze
                 # record step in Path
             #check final step, if == end_point, self.path = path
