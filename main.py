b0421cbc932658f51f81fa5572e9f7537ce1cfb4
priandey <riandeypierre@gmail.com>
2019-01-04 11:59:54 +0100

    minor changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import models as m
 def main():
     maze = m.Maze()
     maze.std_structure()
-    #maze.print_structure(True) # Passer "True" en parametre pour afficher les coordonnées de chaque passage au lieu de '#'
+    maze.print_structure(True) # Passer "True" en parametre pour afficher les coordonnées de chaque passage au lieu de '#'
     maze.get_end_point()
     #maze.print_structure()
     mice = m.Mice(maze)
